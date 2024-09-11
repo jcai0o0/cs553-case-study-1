9fb5db8acdbd7340a5e4f9a8bb991648afdc99d7
Kaitlin Kartsen <katiekarts@icloud.com>
2024-09-11 16:14:06 -0400

    Create tests.py
    Probably has a lot of errors

---
+++ tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestModel(unittest.TestCase):
+
+  def test_cancel(self):
+    cancel_inference()
+    self.assertTrue(stop_inference)
+
+if __name__ == '__main__':
+    unittest.main()
